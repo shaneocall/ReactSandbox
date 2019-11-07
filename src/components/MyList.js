4a730f728d70fb29627fb0a10e0a7de9d5a37393
Shane O'Callaghan <ocallshane@gmail.com>
2019-11-07 16:21:36 -0500

    replace with correct app

---
+++ src/components/MyList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './mylist.css';
+
+function MyList() {
+    return (
+        <div className="MyList">
+            <ul>
+                <li>Never</li>
+                <li>Gonna</li>
+                <li>Give</li>
+                <li>You</li>
+                <li>Up</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MyList;
